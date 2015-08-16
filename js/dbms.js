c6b5f995ead3d24239381aa43d0a69d8c40f508a
NtsDK <ghoul71@yandex.ru>
2015-08-16 05:33:17 +0500

    Fixed character renaming.

--- js/dbms.js
+++ js/dbms.js
@@ -1,14 +1,36 @@
 DBMS = {};
 
-DBMS.createCharacter = function(name){
-	Database.Characters[name] = {name: name, bio:""};
+DBMS.createCharacter = function(name) {
+	Database.Characters[name] = {
+		name : name,
+		bio : ""
+	};
 };
 
-DBMS.renameCharacter = function(fromName, toName){
+DBMS.renameCharacter = function(fromName, toName) {
 	var data = Database.Characters[fromName];
 	data.name = toName;
 	Database.Characters[toName] = data;
 	delete Database.Characters[fromName];
+
+	for ( var storyName in Database.Stories) {
+		var story = Database.Stories[storyName];
+		if (story.characters[fromName]) {
+			var data = story.characters[fromName];
+			data.name = toName;
+			story.characters[toName] = data;
+			delete story.characters[fromName];
+
+			for (var i = 0; i < story.events.length; ++i) {
+				var event = story.events[i];
+				if (event.characters[fromName]) {
+					var data = event.characters[fromName];
+					event.characters[toName] = data;
+					delete event.characters[fromName];
+				}
+			}
+		}
+	}
 };
 
 DBMS.removeCharacter = function(name) {
@@ -17,20 +39,20 @@ DBMS.removeCharacter = function(name) {
 
 DBMS.getCharacterNamesArray = function() {
 	var characterArray = [];
-	
+
 	for ( var name in Database.Characters) {
 		characterArray.push(name);
 	}
-	
+
 	characterArray.sort(charOrdA);
 	return characterArray;
 };
 
 DBMS.getStoryCharacterNamesArray = function(storyName) {
 	var characterArray = [];
-	
+
 	var localCharacters;
-	if(storyName == null){
+	if (storyName == null) {
 		localCharacters = Stories.CurrentStory.characters;
 	} else {
 		localCharacters = Database.Stories[storyName].characters;
@@ -38,18 +60,18 @@ DBMS.getStoryCharacterNamesArray = function(storyName) {
 	for ( var name in localCharacters) {
 		characterArray.push(name);
 	}
-	
+
 	characterArray.sort(charOrdA);
 	return characterArray;
 };
 
 DBMS.getStoryNamesArray = function(storyName) {
 	var stroyNamesArray = [];
-	
+
 	for ( var name in Database.Stories) {
 		stroyNamesArray.push(name);
 	}
-	
+
 	stroyNamesArray.sort(charOrdA);
 	return stroyNamesArray;
 };
