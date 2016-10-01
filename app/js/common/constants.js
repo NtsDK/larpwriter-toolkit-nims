542ff8f47efb1892114bf0adbc648948b667c9b9
Timofey Rechkalov <trechkalov@yandex.ru>
2016-10-02 03:39:08 +0500

    Added en vis network locale.

--- app/js/common/constants.js
+++ app/js/common/constants.js
@@ -268,9 +268,26 @@ See the License for the specific language governing permissions and
             createEdgeError : 'Невозможно соединить ребра в кластер.',
             deleteClusterError : 'Кластеры не могут быть удалены',
             editClusterError : 'Кластеры недоступны для редактирования.'
+        },
+        'en':{
+            edit: 'Edit',
+            del: 'Delete selected',
+            back: 'Back',
+            addNode: 'Add Node',
+            addEdge: 'Add Edge',
+            editNode: 'Edit Node',
+            editEdge: 'Edit Edge',
+            addDescription: 'Click in an empty space to place a new node.',
+            edgeDescription: 'Click on a node and drag the edge to another node to connect them.',
+            editEdgeDescription: 'Click on the control points and drag them to a node to connect to it.',
+            createEdgeError: 'Cannot link edges to a cluster.',
+            deleteClusterError: 'Clusters cannot be deleted.',
+            editClusterError: 'Clusters cannot be edited.'
         }
     };
     
+    exports.visLocales['en_EN'] = exports.visLocales['en'];
+    exports.visLocales['en_US'] = exports.visLocales['en'];
     exports.visLocales['ru_RU'] = exports.visLocales['ru'];
 
     
