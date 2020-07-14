df063771dc1321993176847210acc8e8e593db38
NtsDK <trechkalov@yandex.ru>
2020-07-14 18:56:31 +0200

    Added babel preset for react and  plugin-proposal-class-properties. Updated local permission informer.

--- packages/nims-app/app/front-db/localPermissionInformer.js
+++ packages/nims-app/app/front-db/localPermissionInformer.js
@@ -1,35 +1,41 @@
-exports.refresh = () => Promise.resolve();
+class LocalPermissionInformer {
+    refresh = () => Promise.resolve();
 
-exports.isAdmin = () => Promise.resolve(true);
+    isAdmin = () => Promise.resolve(true);
 
-exports.isEditor = () => Promise.resolve(true);
+    isEditor = () => Promise.resolve(true);
 
-exports.getEntityNamesArray = ({ type, editableOnly } = {}) => new Promise((resolve, reject) => {
-    // function processNames(err, names) {
-    //     if (err) { UI.handleError(err); return; }
-    // }
-    // DBMS.getEntityNamesArray(type, processNames);
-    const Utils2 = UI;
-    DBMS.getEntityNamesArray({ type }).then((names) => {
-        const newNames = [];
-        names.forEach((name) => {
-            newNames.push({
-                displayName: name,
-                value: name,
-                editable: true
+    getEntityNamesArray = ({ type, editableOnly } = {}) => new Promise((resolve, reject) => {
+        // function processNames(err, names) {
+        //     if (err) { UI.handleError(err); return; }
+        // }
+        // DBMS.getEntityNamesArray(type, processNames);
+        const Utils2 = UI;
+        DBMS.getEntityNamesArray({ type }).then((names) => {
+            const newNames = [];
+            names.forEach((name) => {
+                newNames.push({
+                    displayName: name,
+                    value: name,
+                    editable: true
+                });
             });
-        });
-        resolve(newNames);
-    }).catch(Utils2.handleError);
-});
-
-exports.isEntityEditable = ({ type, name } = {}) => Promise.resolve(true);
-
-exports.areAdaptationsEditable = ({ adaptations } = {}) => {
-    const map = {};
-    adaptations.forEach((elem) => {
-        map[`${elem.storyName}-${elem.characterName}`] = true;
+            resolve(newNames);
+        }).catch(Utils2.handleError);
     });
 
-    return Promise.resolve(map);
-};
+    isEntityEditable = ({ type, name } = {}) => Promise.resolve(true);
+
+    areAdaptationsEditable = ({ adaptations } = {}) => {
+        const map = {};
+        adaptations.forEach((elem) => {
+            map[`${elem.storyName}-${elem.characterName}`] = true;
+        });
+
+        return Promise.resolve(map);
+    };
+}
+
+const localPermissionInformer = new LocalPermissionInformer();
+
+export default localPermissionInformer;
