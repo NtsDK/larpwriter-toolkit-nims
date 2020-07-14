2daabd13e201056285393611acd4babebec7742f
NtsDK <trechkalov@yandex.ru>
2020-07-14 20:29:46 +0200

    Moved app-core index.js in js dir.

---
+++ packages/nims-app-core/js/index.js
@@ -0,0 +1,6 @@
+exports.L10n = require('./l10n');
+exports.UI = require('./uiUtils');
+exports.FileUtils = require('./fileUtils');
+exports.TestUtils = require('./testUtils');
+exports.LocalBackupCore = require('./localAutoSave');
+exports.U = require('./utils');
