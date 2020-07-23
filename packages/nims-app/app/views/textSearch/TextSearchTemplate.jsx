5dff93541479b77c2ef599526eb17fb2f358ecb3
NtsDK <trechkalov@yandex.ru>
2020-07-24 01:27:48 +0200

    Text search linting update.

--- packages/nims-app/app/views/textSearch/TextSearchTemplate.jsx
+++ packages/nims-app/app/views/textSearch/TextSearchTemplate.jsx
@@ -1,56 +1,55 @@
 import React from 'react';
 
-export const TextSearchTemplate = function() {
+export const TextSearchTemplate = function () {
   return (
     <div className="text-search-tab block">
       <div className="flex-row">
         <div className="settings-panel">
           <div className="panel panel-default">
             <div className="panel-heading">
-              <h3 className="panel-title" l10n-id="text-search-search"></h3>
+              <h3 className="panel-title" l10n-id="text-search-search" />
             </div>
             <div className="panel-body panel-resizable">
               <div className="margin-bottom-8">
-                <input className="text-search-input form-control" l10n-placeholder-id="text-search-enter-search-string"></input>
+                <input className="text-search-input form-control" l10n-placeholder-id="text-search-enter-search-string" />
               </div>
               <div>
-                <input type="checkbox" className="textSearchTypeRadio hidden" value="characterProfiles" id="characterProfilesTextSearch"/>
-                <label htmlFor="characterProfilesTextSearch" className="checkbox-label-icon common-checkbox"><span l10n-id="text-search-characterProfiles"></span></label> 
+                <input type="checkbox" className="textSearchTypeRadio hidden" value="characterProfiles" id="characterProfilesTextSearch" />
+                <label htmlFor="characterProfilesTextSearch" className="checkbox-label-icon common-checkbox"><span l10n-id="text-search-characterProfiles" /></label>
               </div>
               <div>
-                <input type="checkbox" className="textSearchTypeRadio hidden" value="playerProfiles" id="playerProfilesTextSearch"/>
-                <label htmlFor="playerProfilesTextSearch" className="checkbox-label-icon common-checkbox"><span l10n-id="text-search-playerProfiles"></span></label> 
+                <input type="checkbox" className="textSearchTypeRadio hidden" value="playerProfiles" id="playerProfilesTextSearch" />
+                <label htmlFor="playerProfilesTextSearch" className="checkbox-label-icon common-checkbox"><span l10n-id="text-search-playerProfiles" /></label>
               </div>
               <div>
-                <input type="checkbox" className="textSearchTypeRadio hidden" value="groups" id="groupsTextSearch"/>
-                <label htmlFor="groupsTextSearch" className="checkbox-label-icon common-checkbox"><span l10n-id="text-search-groups"></span></label> 
+                <input type="checkbox" className="textSearchTypeRadio hidden" value="groups" id="groupsTextSearch" />
+                <label htmlFor="groupsTextSearch" className="checkbox-label-icon common-checkbox"><span l10n-id="text-search-groups" /></label>
               </div>
               <div>
-                <input type="checkbox" className="textSearchTypeRadio hidden" value="relations" id="relationsTextSearch"/>
-                <label htmlFor="relationsTextSearch" className="checkbox-label-icon common-checkbox"><span l10n-id="text-search-relations"></span></label> 
+                <input type="checkbox" className="textSearchTypeRadio hidden" value="relations" id="relationsTextSearch" />
+                <label htmlFor="relationsTextSearch" className="checkbox-label-icon common-checkbox"><span l10n-id="text-search-relations" /></label>
               </div>
               <div>
-                <input type="checkbox" className="textSearchTypeRadio hidden" value="writerStory" id="writerStoryTextSearch"/>
-                <label htmlFor="writerStoryTextSearch" className="checkbox-label-icon common-checkbox"><span l10n-id="text-search-writerStory"></span></label> 
+                <input type="checkbox" className="textSearchTypeRadio hidden" value="writerStory" id="writerStoryTextSearch" />
+                <label htmlFor="writerStoryTextSearch" className="checkbox-label-icon common-checkbox"><span l10n-id="text-search-writerStory" /></label>
               </div>
               <div>
-                <input type="checkbox" className="textSearchTypeRadio hidden" value="eventOrigins" id="eventOriginsTextSearch"/>
-                <label htmlFor="eventOriginsTextSearch" className="checkbox-label-icon common-checkbox"><span l10n-id="text-search-eventOrigins"></span></label> 
+                <input type="checkbox" className="textSearchTypeRadio hidden" value="eventOrigins" id="eventOriginsTextSearch" />
+                <label htmlFor="eventOriginsTextSearch" className="checkbox-label-icon common-checkbox"><span l10n-id="text-search-eventOrigins" /></label>
               </div>
               <div className="margin-bottom-16">
-                <input type="checkbox" className="textSearchTypeRadio hidden" value="eventAdaptations" id="eventAdaptationsTextSearch"/>
-                <label htmlFor="eventAdaptationsTextSearch" className="checkbox-label-icon common-checkbox"><span l10n-id="text-search-eventAdaptations"></span></label> 
+                <input type="checkbox" className="textSearchTypeRadio hidden" value="eventAdaptations" id="eventAdaptationsTextSearch" />
+                <label htmlFor="eventAdaptationsTextSearch" className="checkbox-label-icon common-checkbox"><span l10n-id="text-search-eventAdaptations" /></label>
               </div>
               <div className="margin-bottom-16">
-                <input type="checkbox" value="eventAdaptations" className="hidden" id="caseSensitiveTextSearch"/>
-                <label htmlFor="caseSensitiveTextSearch" className="checkbox-label-icon common-checkbox"><span l10n-id="text-search-case-sensitive-search"></span></label> 
+                <input type="checkbox" value="eventAdaptations" className="hidden" id="caseSensitiveTextSearch" />
+                <label htmlFor="caseSensitiveTextSearch" className="checkbox-label-icon common-checkbox"><span l10n-id="text-search-case-sensitive-search" /></label>
               </div>
-              <button className="text-search-button btn btn-default" l10n-id="text-search-find"></button>
+              <button type="button" className="text-search-button btn btn-default" l10n-id="text-search-find" />
             </div>
           </div>
         </div>
-        <div className="result-panel">
-        </div>
+        <div className="result-panel" />
       </div>
     </div>
   );
--- packages/nims-app/app/views/textSearch/textSearch.js
+++ packages/nims-app/app/views/textSearch/textSearch.js
@@ -1,71 +1,71 @@
 import ReactDOM from 'react-dom';
-import { getTextSearchTemplate } from "./TextSearchTemplate.jsx";
 import { UI, U, L10n } from 'nims-app-core';
+import * as CU from 'nims-dbms-core/commonUtils';
+import * as R from 'ramda';
+import { getTextSearchTemplate } from './TextSearchTemplate.jsx';
 
 const root = '.text-search-tab ';
 
 export class TextSearch {
-
     content;
 
-    constructor({L10n, DBMS}) {
-        this.findTexts = this.findTexts.bind(this);
-        this.L10nObj = L10n;
-        this.DBMSObj = DBMS;
+    constructor({ L10n, DBMS }) {
+      this.findTexts = this.findTexts.bind(this);
+      this.L10nObj = L10n;
+      this.DBMSObj = DBMS;
     }
 
-    getContent(){
-        return this.content;
+    getContent() {
+      return this.content;
     }
 
-    init(){
-        this.content = U.makeEl('div');
-        U.addEl(U.qe('.tab-container'), this.content);
-        ReactDOM.render(getTextSearchTemplate(), this.content);
-        this.L10nObj.localizeStatic(this.content);
+    init() {
+      this.content = U.makeEl('div');
+      U.addEl(U.qe('.tab-container'), this.content);
+      ReactDOM.render(getTextSearchTemplate(), this.content);
+      this.L10nObj.localizeStatic(this.content);
 
-        U.listen(U.queryEl(`${root}.text-search-button`), 'click', this.findTexts);
-        U.listenOnEnter(U.queryEl(`${root}.text-search-input`), this.findTexts);
-        this.content = U.queryEl(root);
-    };
+      U.listen(U.queryEl(`${root}.text-search-button`), 'click', this.findTexts);
+      U.listenOnEnter(U.queryEl(`${root}.text-search-input`), this.findTexts);
+      this.content = U.queryEl(root);
+    }
 
     refresh() {
-    };
-
-    findTexts() {
-        const selectedTextTypes = U.queryElEls(U.queryEl(root), `${root}.textSearchTypeRadio`)
-            .filter(el => el.checked).map(el => el.value);
-        const searchStr = U.queryEl(`${root}.text-search-input`).value;
-        const caseSensitive = U.queryEl('#caseSensitiveTextSearch').checked;
-        this.DBMSObj.getTexts({
-            searchStr,
-            textTypes: selectedTextTypes,
-            caseSensitive
-        }).then((texts) => {
-            const text2panel = text => makePanel(
-                U.makeText(`${this.L10nObj.getValue(`text-search-${text.textType}`)} (${text.result.length})`),
-                makePanelContent(text, searchStr, caseSensitive)
-            );
-            U.addEls(U.clearEl(U.queryEl(`${root}.result-panel`)), texts.map(text2panel));
-        }).catch(UI.handleError);
     }
 
+    findTexts() {
+      const selectedTextTypes = U.queryElEls(U.queryEl(root), `${root}.textSearchTypeRadio`)
+        .filter((el) => el.checked).map((el) => el.value);
+      const searchStr = U.queryEl(`${root}.text-search-input`).value;
+      const caseSensitive = U.queryEl('#caseSensitiveTextSearch').checked;
+      this.DBMSObj.getTexts({
+        searchStr,
+        textTypes: selectedTextTypes,
+        caseSensitive
+      }).then((texts) => {
+        const text2panel = (text) => makePanel(
+          U.makeText(`${this.L10nObj.getValue(`text-search-${text.textType}`)} (${text.result.length})`),
+          makePanelContent(text, searchStr, caseSensitive)
+        );
+        U.addEls(U.clearEl(U.queryEl(`${root}.result-panel`)), texts.map(text2panel));
+      }).catch(UI.handleError);
+    }
 }
 
 function makePanelContent(textsInfo, searchStr, caseSensitive) {
-    textsInfo.result.sort(CU.charOrdAFactory(R.prop('name')));
-    return U.addEls(U.makeEl('div'), textsInfo.result.map((textInfo) => {
-        const head = U.addEl(U.makeEl('div'), U.makeText(textInfo.name));
-        const body = U.addClass(U.makeEl('div'), textInfo.type === 'text' ? 'text-body' : 'string-body');
-        const regex = new RegExp(CU.escapeRegExp(searchStr), caseSensitive ? 'g' : 'gi');
-        body.innerHTML = textInfo.text.replace(regex, '<span>$&</span>');
-        return U.addEls(U.addClass(U.makeEl('div'), 'text-card'), [head, body]);
-    }));
+  textsInfo.result.sort(CU.charOrdAFactory(R.prop('name')));
+  return U.addEls(U.makeEl('div'), textsInfo.result.map((textInfo) => {
+    const head = U.addEl(U.makeEl('div'), U.makeText(textInfo.name));
+    const body = U.addClass(U.makeEl('div'), textInfo.type === 'text' ? 'text-body' : 'string-body');
+    const regex = new RegExp(CU.escapeRegExp(searchStr), caseSensitive ? 'g' : 'gi');
+    body.innerHTML = textInfo.text.replace(regex, '<span>$&</span>');
+    return U.addEls(U.addClass(U.makeEl('div'), 'text-card'), [head, body]);
+  }));
 }
 
 function makePanel(title, content) {
-    const panelInfo = UI.makePanelCore(title, content);
-    UI.attachPanelToggler(panelInfo.a, panelInfo.contentDiv);
-    panelInfo.a.click();
-    return panelInfo.panel;
+  const panelInfo = UI.makePanelCore(title, content);
+  UI.attachPanelToggler(panelInfo.a, panelInfo.contentDiv);
+  panelInfo.a.click();
+  return panelInfo.panel;
 }
