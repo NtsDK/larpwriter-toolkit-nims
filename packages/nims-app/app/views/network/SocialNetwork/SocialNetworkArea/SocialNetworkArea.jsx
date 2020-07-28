b3dea59056ddf003289f0c151f0b2e9db4bd1b0f
NtsDK <trechkalov@yandex.ru>
2020-07-28 03:36:41 +0200

    Initial work on social network drawing.

--- packages/nims-app/app/views/network/SocialNetwork/SocialNetworkArea/SocialNetworkArea.jsx
+++ packages/nims-app/app/views/network/SocialNetwork/SocialNetworkArea/SocialNetworkArea.jsx
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
+import vis from 'vis';
+import 'vis/dist/vis.min.css';
+import PermissionInformer from 'permissionInformer';
+import ReactDOM from 'react-dom';
+import { UI, U, L10n } from 'nims-app-core';
+import * as R from 'ramda';
+import * as CU from 'nims-dbms-core/commonUtils';
+import * as Constants from 'nims-dbms/nimsConstants';
 import './SocialNetworkArea.css';
 
 export class SocialNetworkArea extends Component {
+  nodesDataset = new vis.DataSet();
+
+  edgesDataset = new vis.DataSet();
+
   constructor(props) {
     super(props);
     this.state = {
     };
+    this.networkContainer = React.createRef();
   }
 
   componentDidMount() {
+    this.initNetwork();
     console.log('SocialNetworkArea mounted');
   }
 
@@ -20,8 +34,39 @@ export class SocialNetworkArea extends Component {
     console.log('SocialNetworkArea will unmount');
   }
 
+  initNetwork() {
+    if (this.network === undefined) {
+      // const timeline = new vis.Timeline(this.networkContainer.current, null, options);
+      // timeline.setGroups(this.tagDataset);
+      // timeline.setItems(this.timelineDataset);
+      // this.timeline = timeline;
+      const data = {
+        nodes: this.nodesDataset,
+        edges: this.edgesDataset
+      }; // Note: data is coming from ./datasources/WorldCup2014.js
+      const opts = R.clone(Constants.socialNetworkOpts);
+      // opts.groups = groupColors;
+
+      this.network = new vis.Network(this.networkContainer.current, data, opts);
+    }
+  }
+
+  // redrawAll(groupColors, nodes, edges) {
+  //   this.nodesDataset = new vis.DataSet(nodes);
+  //   this.edgesDataset = new vis.DataSet(edges);
+  //   const container = U.queryEl('#socialNetworkContainer');
+
+  //   if (this.network) {
+  //     this.network.destroy();
+  //   }
+
+  //   this.network.on('click', this.neighbourhoodHighlight);
+  // }
+
   render() {
-    const { something } = this.state;
+    if (this.network !== undefined) {
+      // const { something } = this.state;
+    }
     // const { t } = this.props;
 
     // if (!something) {
@@ -29,7 +74,11 @@ export class SocialNetworkArea extends Component {
     //   // return null;
     // }
     return (
-      <div className="SocialNetworkArea visualObjectContainer full-screen-elem" id="socialNetworkContainer" />
+      <div
+        ref={this.networkContainer}
+        className="SocialNetworkArea visualObjectContainer full-screen-elem"
+        id="socialNetworkContainer"
+      />
     );
   }
 }
