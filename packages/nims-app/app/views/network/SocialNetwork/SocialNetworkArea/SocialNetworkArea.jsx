e69f6c7afc6f5c81e0a67931bd4b9e36fcd1eb75
NtsDK <trechkalov@yandex.ru>
2020-07-31 03:30:27 +0200

    Added more features to social network - focus node, show player names and highlight friends.

--- packages/nims-app/app/views/network/SocialNetwork/SocialNetworkArea/SocialNetworkArea.jsx
+++ packages/nims-app/app/views/network/SocialNetwork/SocialNetworkArea/SocialNetworkArea.jsx
@@ -19,6 +19,7 @@ export class SocialNetworkArea extends Component {
     this.state = {
     };
     this.networkContainer = React.createRef();
+    this.neighbourhoodHighlight = this.neighbourhoodHighlight.bind(this);
   }
 
   componentDidMount() {
@@ -32,7 +33,7 @@ export class SocialNetworkArea extends Component {
 
   componentDidUpdate(prevProps) {
     const {
-      nodes, edges, groupColors, getNodeColorsUpdate, selectedGroup
+      nodes, edges, groupColors, getNodeColorsUpdate, selectedGroup, focusNode
     } = this.props;
     if (nodes !== prevProps.nodes
       || edges !== prevProps.edges) {
@@ -65,6 +66,10 @@ export class SocialNetworkArea extends Component {
     if (selectedGroup !== prevProps.selectedGroup) {
       this.nodesDataset.update(getNodeColorsUpdate());
     }
+    if (focusNode !== prevProps.focusNode) {
+      // this.nodesDataset.update(getNodeColorsUpdate());
+      this.network.focus(focusNode, Constants.snFocusOptions);
+    }
 
     console.log('SocialNetworkArea did update');
   }
@@ -88,7 +93,42 @@ export class SocialNetworkArea extends Component {
       opts.groups = groupColors;
 
       this.network = new vis.Network(this.networkContainer.current, data, opts);
+
+      this.network.on('click', this.neighbourhoodHighlight);
     }
+  }
+
+  // called from redrawAll
+  neighbourhoodHighlight(params) {
+    // if()
+    // get a JSON object
+    const allNodes = this.nodesDataset.get({
+      returnType: 'Object'
+    });
+
+    const { network } = this;
+    if (params.nodes.length > 0) {
+      this.highlightActive = true;
+      const selectedNode = params.nodes[0];
+      const zeroDegreeNodes = [selectedNode];
+      const firstDegreeNodes = network.getConnectedNodes(selectedNode);
+      highlightNodes(network, allNodes, zeroDegreeNodes, firstDegreeNodes);
+    } else if (params.edges.length > 0) {
+      this.highlightActive = true;
+      const selectedEdge = params.edges[0];
+      const firstDegreeNodes = network.getConnectedNodes(selectedEdge);
+      highlightNodes(network, allNodes, [], firstDegreeNodes);
+    } else if (this.highlightActive === true) {
+      // reset all nodes
+      R.values(allNodes).forEach((node) => {
+        node.color = undefined;
+        showLabel(node);
+      });
+      this.highlightActive = false;
+    }
+
+    // transform the object into an array
+    this.nodesDataset.update(R.values(allNodes));
   }
 
   // redrawAll(groupColors, nodes, edges) {
@@ -120,5 +160,44 @@ export class SocialNetworkArea extends Component {
         id="socialNetworkContainer"
       />
     );
+  }
+}
+
+// called from neighbourhoodHighlight
+function highlightNodes(network, allNodes, zeroDegreeNodes, firstDegreeNodes) {
+  // get the second degree nodes
+  const secondDegreeNodes = R.uniq(R.flatten(firstDegreeNodes.map((id) => network.getConnectedNodes(id))));
+  // mark all nodes as hard to read.
+  R.values(allNodes).forEach((node) => {
+    node.color = 'rgba(200,200,200,0.5)';
+    hideLabel(node);
+  });
+  // all second degree nodes get a different color and their label back
+  secondDegreeNodes.map((id) => allNodes[id]).forEach((node) => {
+    node.color = 'rgba(150,150,150,0.75)';
+    showLabel(node);
+  });
+  // all first degree nodes get their own color and their label back
+  firstDegreeNodes.map((id) => allNodes[id]).forEach((node) => {
+    node.color = undefined;
+    showLabel(node);
+  });
+  // the main node gets its own color and its label back.
+  zeroDegreeNodes.map((id) => allNodes[id]).forEach((node) => {
+    node.color = undefined;
+    showLabel(node);
+  });
+}
+
+function hideLabel(node) {
+  if (node.hiddenLabel === undefined) {
+    node.hiddenLabel = node.label;
+    node.label = undefined;
+  }
+}
+function showLabel(node) {
+  if (node.hiddenLabel !== undefined) {
+    node.label = node.hiddenLabel;
+    node.hiddenLabel = undefined;
   }
 }
