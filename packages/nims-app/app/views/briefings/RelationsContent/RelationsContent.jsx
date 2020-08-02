9a1e1f558c58adaac3b83bfb1a793875bcc58df3
NtsDK <trechkalov@yandex.ru>
2020-08-02 02:58:56 +0200

    More work on relations.

--- packages/nims-app/app/views/briefings/RelationsContent/RelationsContent.jsx
+++ packages/nims-app/app/views/briefings/RelationsContent/RelationsContent.jsx
@@ -6,6 +6,11 @@ import ProjectUtils from 'nims-dbms/db-utils/projectUtils';
 import * as CU from 'nims-dbms-core/commonUtils';
 import './RelationsContent.css';
 
+const findRel = R.curry((fromCharacter, toCharacter, relations) => {
+  const findFunc = R.curry((fromCharacter2, toCharacter2, rel) => rel[fromCharacter2] !== undefined && rel[toCharacter2] !== undefined);
+  return R.find(findFunc(fromCharacter, toCharacter), relations);
+});
+
 export class RelationsContent extends Component {
   constructor(props) {
     super(props);
@@ -15,8 +20,16 @@ export class RelationsContent extends Component {
       characterNamesArray: null,
       profileBindings: null,
       profileItemNames: null,
-      selectedProfileItem: null
+      selectedProfileItem: null,
+      showCharacters: null,
+      knownByStoriesNoRels: null,
+      selectedKnownCharacter: null,
+      unknownByStoriesNoRels: null,
+      selectedUnknownCharacter: null
     };
+    this.onKnownCharacterChange = this.onKnownCharacterChange.bind(this);
+    this.onUnknownCharacterChange = this.onUnknownCharacterChange.bind(this);
+    this.onSelectedProfileItemChange = this.onSelectedProfileItemChange.bind(this);
   }
 
   componentDidMount() {
@@ -54,13 +67,29 @@ export class RelationsContent extends Component {
       // data.profiles = profiles;
       // data.profileBindings = R.fromPairs(profileBindings);
       const profileItemNames = characterProfileStructure.map(R.prop('name')).sort();
+
+      const notCharacter = R.compose(R.not, R.equals(characterName), R.prop('value'));
+      const otherCharacters = characterNamesArray.filter(notCharacter);
+
+      const get2ndCharName = ProjectUtils.get2ndRelChar(characterName);
+      const showCharacters = relationsSummary.relations.map(get2ndCharName).sort(CU.charOrdA);
+      const hasRelWithCharacter = R.compose(R.not, R.contains(R.__, showCharacters), R.prop('value'));
+      const noRelsList = otherCharacters.filter(hasRelWithCharacter);
+      const meetInStories = R.compose(R.contains(R.__, R.keys(relationsSummary.knownCharacters)), R.prop('value'));
+      const [knownByStoriesNoRels, unknownByStoriesNoRels] = R.partition(meetInStories, noRelsList);
+
       this.setState({
         profiles,
         relationsSummary,
         characterNamesArray,
         profileBindings: R.fromPairs(profileBindings),
         profileItemNames,
-        selectedProfileItem: profileItemNames.length > 0 ? profileItemNames[0] : null
+        selectedProfileItem: profileItemNames.length > 0 ? profileItemNames[0] : null,
+        showCharacters,
+        knownByStoriesNoRels,
+        selectedKnownCharacter: knownByStoriesNoRels.length > 0 ? knownByStoriesNoRels[0].value : null,
+        unknownByStoriesNoRels,
+        selectedUnknownCharacter: unknownByStoriesNoRels.length > 0 ? unknownByStoriesNoRels[0].value : null,
       });
 
       // callback();
@@ -136,6 +165,7 @@ export class RelationsContent extends Component {
     //   U.addEl(tmplQe('.unknown-characters-label'), U.makeText(l10n('unknown-characters')));
     //   const unknownBtn = U.addEl(tmplQe('.add-unknown-character-relation'), U.makeText(L10n.getValue('common-add')));
     //   U.addEl(tmplQe('.profile-item-label'), U.makeText(l10n('profile-item')));
+
     //   fillCharSelector(tmplQe('.known-characters-select'), knownBtn, knownByStoriesNoRels, characterName, makeRowCallback);
     //   fillCharSelector(tmplQe('.unknown-characters-select'), unknownBtn, unknownByStoriesNoRels, characterName, makeRowCallback);
 
@@ -147,9 +177,155 @@ export class RelationsContent extends Component {
   //   return relationTmpl;
   }
 
+  // function fillCharSelector(select1, button, data, fromCharacter, makeRowCallback) {
+  //   select1 = $(select1);
+  //   const tmpSelect = select1.select2(UI.getSelect2Data(data));
+  //   select1.select2({ width: 'style' });
+  //   U.listen(button, 'click', () => {
+  //     const toCharacter = select1[0].value;
+  //     DBMS.createCharacterRelation({ fromCharacter, toCharacter }).then(() => {
+  //       DBMS.getCharacterRelation({ fromCharacter, toCharacter }).then((rel) => {
+  //         makeRowCallback(select1[0].value, rel);
+  //         data = data.filter(R.compose(R.not, R.equals(select1[0].value), R.prop('value')));
+  //         U.clearEl(select1[0]);
+  //         select1.select2(UI.getSelect2Data(data));
+  //       }).catch(UI.handleError);
+  //     }).catch(UI.handleError);
+  //   });
+  // }
+
+  // makeNewRow = R.curry((
+  //   profiles, getProfileItemSelect, isAdaptationsMode, knownCharacters, profileBindings,
+  //   externalRefresh, fromCharacter, toCharacter, rel
+  // ) => {
+  //   const stories = knownCharacters[toCharacter];
+
+  //   const content = U.makeEl('div');
+  //   ReactDOM.render(getRelationRow(), content);
+  //   const row = U.qee(content, '.RelationRow');
+
+  //   // const row = U.qmte('.relation-row-tmpl');
+  //   const tmplQe = U.qee(row);
+  //   U.addEl(tmplQe('.to-character-name'), U.makeText(`${toCharacter}/${profileBindings[toCharacter]}`));
+  //   U.addEl(tmplQe('.where-meets-label'), U.makeText(l10n('where-meets')));
+  //   U.addEl(tmplQe('.where-meets-content'), U.makeText(stories === undefined ? '' : R.keys(stories).join(', ')));
+  //   U.setAttr(tmplQe('[toCharacter]'), 'toCharacter', toCharacter);
+  //   fillProfileItemContent(row, getProfileItemSelect().value, profiles[toCharacter][getProfileItemSelect().value]);
+  //   U.listen(tmplQe('button.remove'), 'click', (event) => {
+  //     UI.confirm(CU.strFormat(l10n('are-you-sure-about-relation-removing'), [`${`${fromCharacter}-${toCharacter}`}`]), () => {
+  //       DBMS.removeCharacterRelation({ fromCharacter, toCharacter }).then(externalRefresh).catch(UI.handleError);
+  //     });
+  //   });
+
+  //   const directText = tmplQe('.direct textarea');
+  //   directText.value = rel[fromCharacter];
+  //   U.setAttr(directText, 'placeholder', L10n.format('briefings', 'relation-from-to', [fromCharacter, toCharacter]));
+  //   U.listen(directText, 'change', (event) => {
+  //     DBMS.setCharacterRelationText({
+  //       fromCharacter,
+  //       toCharacter,
+  //       character: fromCharacter,
+  //       text: event.target.value
+  //     }).catch(UI.handleError);
+  //   });
+
+  //   Constants.relationEssences.forEach((name) => {
+  //     const btn = tmplQe(`.${name}`);
+  //     $(btn).tooltip({
+  //       title: L10n.format('briefings', `${name}`, [fromCharacter, toCharacter]),
+  //       placement: 'top'
+  //     });
+  //     let attrName = name;
+  //     if (rel.starter !== fromCharacter) {
+  //       if (name === 'starterToEnder') attrName = 'enderToStarter';
+  //       if (name === 'enderToStarter') attrName = 'starterToEnder';
+  //     }
+  //     U.setClassByCondition(btn, 'btn-primary', rel.essence.indexOf(attrName) !== -1);
+  //     U.listen(btn, 'click', (event) => {
+  //       DBMS.setRelationEssenceStatus({
+  //         fromCharacter,
+  //         toCharacter,
+  //         essence: attrName,
+  //         flag: !U.hasClass(event.target, 'btn-primary')
+  //       }).then(() => {
+  //         U.toggleClass(event.target, 'btn-primary');
+  //       }).catch(UI.handleError);
+  //     });
+  //   });
+
+  //   const originText = tmplQe('.origin textarea');
+  //   originText.value = rel.origin;
+  //   U.setAttr(originText, 'placeholder', l10n('relation-origin'));
+  //   U.listen(originText, 'change', (event) => {
+  //     DBMS.setOriginRelationText({
+  //       fromCharacter,
+  //       toCharacter,
+  //       text: event.target.value
+  //     }).catch(UI.handleError);
+  //   });
+
+  //   const reverseText = tmplQe('.reverse textarea');
+  //   reverseText.value = rel[toCharacter];
+  //   U.setAttr(reverseText, 'placeholder', L10n.format('briefings', 'relation-from-to', [toCharacter, fromCharacter]));
+  //   U.listen(reverseText, 'change', (event) => {
+  //     DBMS.setCharacterRelationText({
+  //       fromCharacter,
+  //       toCharacter,
+  //       character: toCharacter,
+  //       text: event.target.value
+  //     }).catch(UI.handleError);
+  //   });
+
+  //   const directChecked = rel.starter === fromCharacter ? rel.starterTextReady : rel.enderTextReady;
+  //   fillFinishedButton(
+  //     tmplQe('.direct .finished'), JSON.stringify([fromCharacter, toCharacter]), fromCharacter,
+  //     toCharacter, fromCharacter, directChecked, directText
+  //   );
+
+  //   const reverseChecked = rel.starter === toCharacter ? rel.starterTextReady : rel.enderTextReady;
+  //   fillFinishedButton(
+  //     tmplQe('.reverse .finished'), JSON.stringify([toCharacter, fromCharacter]), fromCharacter,
+  //     toCharacter, toCharacter, reverseChecked, reverseText
+  //   );
+
+  //   if (!isAdaptationsMode) {
+  //     U.removeClass(tmplQe('.direct'), 'col-xs-3');
+  //     U.addClass(tmplQe('.direct'), 'col-xs-9');
+  //     U.addClass(tmplQe('.origin'), 'hidden');
+  //     U.addClass(tmplQe('.reverse'), 'hidden');
+  //   }
+  //   L10n.localizeStatic(row);
+
+  //   return row;
+  // });
+
+  onKnownCharacterChange(e) {
+    this.setState({
+      selectedKnownCharacter: e.target.value
+    });
+  }
+
+  onUnknownCharacterChange(e) {
+    this.setState({
+      selectedUnknownCharacter: e.target.value
+    });
+  }
+
+  onSelectedProfileItemChange(e) {
+    this.setState({
+      selectedProfileItem: e.target.value
+    });
+  }
+
   render() {
     const {
-      relationsSummary, profiles, profileBindings, characterNamesArray, profileItemNames, selectedProfileItem
+      relationsSummary, profiles, profileBindings, characterNamesArray,
+      profileItemNames, selectedProfileItem,
+      showCharacters,
+      knownByStoriesNoRels,
+      unknownByStoriesNoRels,
+      selectedKnownCharacter,
+      selectedUnknownCharacter
     } = this.state;
     const {
       t, characterName, isAdaptationsMode, characterProfileStructure
@@ -160,46 +336,136 @@ export class RelationsContent extends Component {
       return null;
     }
     this.makeRelationsContent();
-    // const {
-    //   relationsSummary, profiles, profileBindings, characterNamesArray
-    // } = this.state;
-    // const {
-    //   t, characterName, isAdaptationsMode, characterProfileStructure
-    // } = this.props;
-    const profileSettings = characterProfileStructure;
 
-    const notCharacter = R.compose(R.not, R.equals(characterName), R.prop('value'));
-    const otherCharacters = characterNamesArray.filter(notCharacter);
-
-    const get2ndCharName = ProjectUtils.get2ndRelChar(characterName);
-    const showCharacters = relationsSummary.relations.map(get2ndCharName).sort(CU.charOrdA);
-    const hasRelWithCharacter = R.compose(R.not, R.contains(R.__, showCharacters), R.prop('value'));
-    const noRelsList = otherCharacters.filter(hasRelWithCharacter);
-    const meetInStories = R.compose(R.contains(R.__, R.keys(relationsSummary.knownCharacters)), R.prop('value'));
-    const [knownByStoriesNoRels, unknownByStoriesNoRels] = R.partition(meetInStories, noRelsList);
-
+    const toCharacterFilter = (toCharacter) => (isAdaptationsMode ? true
+      : !R.isEmpty(findRel(characterName, toCharacter, relationsSummary.relations)[characterName]));
+    const findRelTmp = findRel(characterName, R.__, relationsSummary.relations);
+    // U.addEls(content, showCharacters.filter(toCharacterFilter).map((toChar) => makeRow(toChar, findRelTmp(toChar))));
     return (
       <div className="RelationsContent">
-        {characterName}
+
         <div className="Relation entity-management relations-management">
           <div>
-            <span className="known-characters-label" />
-            <span><select className="common-select known-characters-select" style={{ width: '200px' }} /></span>
-            <button type="button" className="add-known-character-relation btn btn-default btn-reduced" />
+            <span className="known-characters-label">{t('briefings.known-characters')}</span>
+            <span>
+              <select
+                className="common-select known-characters-select"
+                value={selectedKnownCharacter}
+                style={{ width: '200px' }}
+                onChange={this.onKnownCharacterChange}
+              >
+                {
+                  knownByStoriesNoRels.map((character) => (
+                    <option
+                      key={character.value}
+                      value={character.value}
+                    >
+                      {character.displayName}
+                    </option>
+                  ))
+                }
+              </select>
+            </span>
+            <button type="button" className="add-known-character-relation btn btn-default btn-reduced">{t('common.add')}</button>
           </div>
           <div>
-            <span className="unknown-characters-label" />
-            <span><select className="common-select unknown-characters-select" style={{ width: '200px' }} /></span>
-            <button type="button" className="add-unknown-character-relation btn btn-default btn-reduced" />
+            <span className="unknown-characters-label">{t('briefings.unknown-characters')}</span>
+            <span>
+              <select
+                className="common-select unknown-characters-select"
+                value={selectedUnknownCharacter}
+                style={{ width: '200px' }}
+                onChange={this.onUnknownCharacterChange}
+              >
+                {
+                  unknownByStoriesNoRels.map((character) => (
+                    <option
+                      key={character.value}
+                      value={character.value}
+                    >
+                      {character.displayName}
+                    </option>
+                  ))
+                }
+              </select>
+            </span>
+            <button type="button" className="add-unknown-character-relation btn btn-default btn-reduced">{t('common.add')}</button>
           </div>
           <div>
-            <span className="profile-item-label" />
-            <span><select className="common-select profile-item-select" style={{ width: '200px' }} /></span>
+            <span className="profile-item-label">{t('briefings.profile-item')}</span>
+            <span>
+              <select
+                className="common-select profile-item-select"
+                value={selectedProfileItem}
+                style={{ width: '200px' }}
+                onChange={this.onSelectedProfileItemChange}
+              >
+                {
+                  profileItemNames.map((name) => <option key={name} value={name}>{name}</option>)
+                }
+              </select>
+            </span>
           </div>
         </div>
 
-        <div className="relation-content container-fluid" />
+        <div className="relation-content container-fluid">
+          {
+            showCharacters.filter(toCharacterFilter).map((toChar) => (
+              <div className="RelationRow row">
+                <div className="to-character-data col-xs-3">
+                  <h4 className="to-character-name" />
+                  <div>
+                    <div className="where-meets-label bold-cursive">{t('briefings.where-meets')}</div>
+                    <div className="where-meets-content" />
+                  </div>
+                  <div tocharacter="">
+                    <div className="profile-item-name bold-cursive" />
+                    <div className="profile-item-value" />
+                  </div>
+                  <div>
+                    <button type="button" className="btn btn-default fa-icon remove" />
+                  </div>
+                </div>
+                <div className="direct text-column col-xs-3">
+                  <div className="pre-text-area">
+                    <button type="button" className="btn btn-default fa-icon finished" title={t('constant.finishedText')} />
+                  </div>
+                  <textarea className="briefing-relation-area form-control" />
+                </div>
+                <div className="origin text-column col-xs-3">
+                  <div className="pre-text-area btn-group">
+                    <button type="button" className="btn btn-default fa-icon starterToEnder" />
+                    <button type="button" className="btn btn-default fa-icon allies" />
+                    <button type="button" className="btn btn-default fa-icon enderToStarter" />
+                  </div>
+                  <textarea className="briefing-relation-area form-control" placeholder={t('briefings.relation-origin')} />
+                </div>
+                <div className="reverse text-column col-xs-3">
+                  <div className="pre-text-area">
+                    <button type="button" className="btn btn-default fa-icon finished" title={t('constant.finishedText')} />
+                  </div>
+                  <textarea className="briefing-relation-area form-control" />
+                </div>
+              </div>
+            ))
+          }
+        </div>
       </div>
     );
+
+    // UI.confirm(CU.strFormat(l10n('are-you-sure-about-relation-removing'), [`${`${fromCharacter}-${toCharacter}`}`]), () => {
+
+    //   const directText = tmplQe('.direct textarea');
+    //   directText.value = rel[fromCharacter];
+    //   U.setAttr(directText, 'placeholder', L10n.format('briefings', 'relation-from-to', [fromCharacter, toCharacter]));
+
+    //   Constants.relationEssences.forEach((name) => {
+    //     const btn = tmplQe(`.${name}`);
+    //     $(btn).tooltip({
+    //       title: L10n.format('briefings', `${name}`, [fromCharacter, toCharacter]),
+
+    //   const reverseText = tmplQe('.reverse textarea');
+    //   reverseText.value = rel[toCharacter];
+    //   U.setAttr(reverseText, 'placeholder', L10n.format('briefings', 'relation-from-to', [toCharacter, fromCharacter]));
   }
 }
