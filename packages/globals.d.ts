903a922585d02a593215b7eb3439ef192045d665
NtsDK <trechkalov@yandex.ru>
2020-08-18 15:45:03 +0200

    Added TS declarations for webpack global constants.

---
+++ packages/globals.d.ts
@@ -0,0 +1,9 @@
+declare var PRODUCT: string;
+declare var MODE: string;
+declare var PROJECT_NAME: string;
+declare var DEV_OPTS: {
+  ENABLE_TESTS: boolean;
+  ENABLE_BASE_SELECT_DLG: boolean;
+  ENABLE_BASICS: boolean;
+  ENABLE_EXTRAS: boolean;
+};
