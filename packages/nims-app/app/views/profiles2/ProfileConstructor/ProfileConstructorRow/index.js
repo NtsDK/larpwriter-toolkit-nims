c46e69e16d87df613b7b972472196dd8225424f4
NtsDK <trechkalov@yandex.ru>
2020-08-18 15:44:25 +0200

    Added editing of profile items: do export, change profile item type and player visibility.

--- packages/nims-app/app/views/profiles2/ProfileConstructor/ProfileConstructorRow/index.js
+++ packages/nims-app/app/views/profiles2/ProfileConstructor/ProfileConstructorRow/index.js
@@ -1,7 +1,8 @@
 import { withTranslation } from 'react-i18next';
 import { pipe } from 'ramda';
+import { withDbms } from 'nims-app-core/withDbms.jsx';
 import { ProfileConstructorRow } from './ProfileConstructorRow.jsx';
 
-const tmp = pipe(withTranslation())(ProfileConstructorRow);
+const tmp = pipe(withTranslation(), withDbms)(ProfileConstructorRow);
 
 export { tmp as ProfileConstructorRow };
