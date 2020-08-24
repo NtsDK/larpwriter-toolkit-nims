39aff62d8d9025b64719eea304d2b8518cdc9dcd
NtsDK <trechkalov@yandex.ru>
2020-08-24 02:51:23 +0200

    Added profile binding creation.

--- packages/nims-app/app/views/profiles2/ProfileConstructor/ProfileConstructor.jsx
+++ packages/nims-app/app/views/profiles2/ProfileConstructor/ProfileConstructor.jsx
@@ -111,39 +111,8 @@ export class ProfileConstructor extends Component {
                 }
               </Droppable>
             </DragDropContext>
-            {/* <table className="table table-bordered">
-              <thead>
-                <tr>
-                  <th>№</th>
-                  <th>{t('profiles.table-profile-item-name')}</th>
-                  <th>{t('profiles.profile-item-type')}</th>
-                  <th>{t('profiles.profile-item-default-value')}</th>
-                  <th>{t('profiles.profile-item-player-access')}</th>
-                </tr>
-              </thead>
-              <tbody className="profile-config-container">
-                {
-                  profileStructure.map((profileStructureItem, i) => (
-                    <ProfileConstructorRow profileStructureItem={profileStructureItem} i={i} refresh={this.refresh} />
-                  ))
-                }
-              </tbody>
-            </table> */}
           </div>
         </div>
-
-        {/*
-  <template class="enum-value-editor-tmpl">
-    <div class="flex-row">
-      <div class="margin-right-8 flex-1-1-auto">
-        <span class="text"></span>
-      </div>
-      <div class="flex-0-0-auto">
-        <button class="btn btn-default btn-reduced fa-icon add flex-0-0-auto adminOnly" l10n-title="profiles-add-remove-values"></button>
-        <button class="btn btn-default btn-reduced fa-icon rename flex-0-0-auto adminOnly" l10n-title="profiles-rename-enum-item"></button>
-      </div>
-    </div>
-  </template> */}
       </div>
     );
   }
