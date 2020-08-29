99ace8c801ab6d5611a37784d227e9f34f6e3137
NtsDK <trechkalov@yandex.ru>
2020-08-29 02:44:22 +0200

    Autolinting of profile binding API.

--- packages/nims-dbms/core-apis/profileBindingAPI.js
+++ packages/nims-dbms/core-apis/profileBindingAPI.js
@@ -1,139 +1,124 @@
-/*Copyright 2016 Timofey Rechkalov <ntsdk@yandex.ru>, Maria Sidekhmenova <matilda_@list.ru>
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-    limitations under the License. */
-
-
 /* eslint-disable func-names */
 
 ((callback2) => {
-    function profileBindingAPI(LocalDBMS, opts) {
-        const {
-            R, Constants, Errors, addListener, dbmsUtils, CU, PC
-        } = opts;
+  function profileBindingAPI(LocalDBMS, opts) {
+    const {
+      R, Constants, Errors, addListener, dbmsUtils, CU, PC
+    } = opts;
 
-        const path = ['ProfileBindings'];
-        const charPath = ['Characters'];
-        const playerPath = ['Players'];
+    const path = ['ProfileBindings'];
+    const charPath = ['Characters'];
+    const playerPath = ['Players'];
 
-        LocalDBMS.prototype.getProfileBindings = function () {
-            return Promise.resolve(R.clone(R.path(path, this.database)));
-        };
+    LocalDBMS.prototype.getProfileBindings = function () {
+      return Promise.resolve(R.clone(R.path(path, this.database)));
+    };
 
-        LocalDBMS.prototype.getExtendedProfileBindings = function () {
-            let characters = R.keys(R.path(charPath, this.database));
-            let players = R.keys(R.path(playerPath, this.database));
-            const bindings = R.clone(R.path(path, this.database));
-            characters = R.difference(characters, R.keys(bindings));
-            players = R.difference(players, R.values(bindings));
+    LocalDBMS.prototype.getExtendedProfileBindings = function () {
+      let characters = R.keys(R.path(charPath, this.database));
+      let players = R.keys(R.path(playerPath, this.database));
+      const bindings = R.clone(R.path(path, this.database));
+      characters = R.difference(characters, R.keys(bindings));
+      players = R.difference(players, R.values(bindings));
 
-            const bindingData = R.reduce(R.concat, [], [R.toPairs(bindings),
-                R.zip(characters, R.repeat('', characters.length)),
-                R.zip(R.repeat('', players.length), players)]);
-            return Promise.resolve(bindingData);
-        };
+      const bindingData = R.reduce(R.concat, [], [R.toPairs(bindings),
+        R.zip(characters, R.repeat('', characters.length)),
+        R.zip(R.repeat('', players.length), players)]);
+      return Promise.resolve(bindingData);
+    };
 
-        const _getProfileBinding = (type, name, db) => {
-            let arr;
-            if (type === 'character') {
-                const bindings = R.path(path, db);
-                arr = [name, bindings[name] || ''];
-            } else {
-                const bindings = R.invertObj(R.path(path, db));
-                arr = [bindings[name] || '', name];
-            }
-            return arr;
-        };
+    const _getProfileBinding = (type, name, db) => {
+      let arr;
+      if (type === 'character') {
+        const bindings = R.path(path, db);
+        arr = [name, bindings[name] || ''];
+      } else {
+        const bindings = R.invertObj(R.path(path, db));
+        arr = [bindings[name] || '', name];
+      }
+      return arr;
+    };
 
-        dbmsUtils._getProfileBinding = _getProfileBinding;
+    dbmsUtils._getProfileBinding = _getProfileBinding;
 
-        // DBMS.profileBindings.characters[name].get()
-        LocalDBMS.prototype.getProfileBinding = function ({ type, name } = {}) {
-            return new Promise((resolve, reject) => {
-                const conditions = [PC.isString(type), PC.elementFromEnum(type, Constants.profileTypes), PC.isString(name),
-                    PC.entityExists(name, R.keys(this.database[type === 'character' ? 'Characters' : 'Players']))];
-                PC.precondition(PC.chainCheck(conditions), reject, () => {
-                    resolve(_getProfileBinding(type, name, this.database));
-                });
-            });
-        };
+    // DBMS.profileBindings.characters[name].get()
+    LocalDBMS.prototype.getProfileBinding = function ({ type, name } = {}) {
+      return new Promise((resolve, reject) => {
+        const conditions = [PC.isString(type), PC.elementFromEnum(type, Constants.profileTypes), PC.isString(name),
+          PC.entityExists(name, R.keys(this.database[type === 'character' ? 'Characters' : 'Players']))];
+        PC.precondition(PC.chainCheck(conditions), reject, () => {
+          resolve(_getProfileBinding(type, name, this.database));
+        });
+      });
+    };
 
-        LocalDBMS.prototype.createBinding = function ({ characterName, playerName } = {}) {
-            return new Promise((resolve, reject) => {
-                const bindings = R.path(path, this.database);
-                const conditions = [PC.isString(characterName),
-                    PC.entityExists(characterName, R.keys(this.database.Characters)), PC.isString(playerName),
-                    PC.entityExists(playerName, R.keys(this.database.Players)),
-                    PC.entityIsNotUsed(characterName, R.keys(bindings)),
-                    PC.entityIsNotUsed(playerName, R.keys(R.invertObj(bindings)))];
-                PC.precondition(PC.chainCheck(conditions), reject, () => {
-                    bindings[characterName] = playerName;
-                    resolve();
-                });
-            });
-        };
+    LocalDBMS.prototype.createBinding = function ({ characterName, playerName } = {}) {
+      return new Promise((resolve, reject) => {
+        const bindings = R.path(path, this.database);
+        const conditions = [PC.isString(characterName),
+          PC.entityExists(characterName, R.keys(this.database.Characters)), PC.isString(playerName),
+          PC.entityExists(playerName, R.keys(this.database.Players)),
+          PC.entityIsNotUsed(characterName, R.keys(bindings)),
+          PC.entityIsNotUsed(playerName, R.keys(R.invertObj(bindings)))];
+        PC.precondition(PC.chainCheck(conditions), reject, () => {
+          bindings[characterName] = playerName;
+          resolve();
+        });
+      });
+    };
 
-        LocalDBMS.prototype.removeBinding = function ({ characterName, playerName } = {}) {
-            return new Promise((resolve, reject) => {
-                const bindingArr = R.toPairs(R.path(path, this.database)).map(pair => `${pair[0]}/${pair[1]}`);
-                const conditions = [PC.isString(characterName),
-                    PC.entityExists(characterName, R.keys(this.database.Characters)),
-                    PC.isString(playerName), PC.entityExists(playerName, R.keys(this.database.Players)),
-                    PC.entityExists(`${characterName}/${playerName}`, bindingArr)];
-                PC.precondition(PC.chainCheck(conditions), reject, () => {
-                    delete R.path(path, this.database)[characterName];
-                    resolve();
-                });
-            });
-        };
+    LocalDBMS.prototype.removeBinding = function ({ characterName, playerName } = {}) {
+      return new Promise((resolve, reject) => {
+        const bindingArr = R.toPairs(R.path(path, this.database)).map((pair) => `${pair[0]}/${pair[1]}`);
+        const conditions = [PC.isString(characterName),
+          PC.entityExists(characterName, R.keys(this.database.Characters)),
+          PC.isString(playerName), PC.entityExists(playerName, R.keys(this.database.Players)),
+          PC.entityExists(`${characterName}/${playerName}`, bindingArr)];
+        PC.precondition(PC.chainCheck(conditions), reject, () => {
+          delete R.path(path, this.database)[characterName];
+          resolve();
+        });
+      });
+    };
 
-        function _renameProfile([{ type, fromName, toName }] = []) {
-            const bindings = R.path(path, this.database);
-            if (type === 'character') {
-                const playerName = bindings[fromName];
-                if (playerName !== undefined) {
-                    bindings[toName] = playerName;
-                    delete bindings[fromName];
-                }
-            } else if (type === 'player') {
-                const invertedBindings = R.invertObj(bindings);
-                const characterName = invertedBindings[fromName];
-                if (characterName !== undefined) {
-                    bindings[characterName] = toName;
-                }
-            } else {
-                console.log(`binding._renameProfile: Unexpected type ${type}`);
-            }
+    function _renameProfile([{ type, fromName, toName }] = []) {
+      const bindings = R.path(path, this.database);
+      if (type === 'character') {
+        const playerName = bindings[fromName];
+        if (playerName !== undefined) {
+          bindings[toName] = playerName;
+          delete bindings[fromName];
         }
-
-        addListener('renameProfile', _renameProfile);
-
-        function _removeProfile([{ type, characterName }] = []) {
-            const bindings = R.path(path, this.database);
-            if (type === 'character') {
-                delete bindings[characterName];
-            } else if (type === 'player') {
-                const invertedBindings = R.invertObj(bindings);
-                const characterName2 = invertedBindings[characterName];
-                if (characterName2 !== undefined) {
-                    delete bindings[characterName2];
-                }
-            } else {
-                console.log(`binding._removeProfile: Unexpected type ${type}`);
-            }
+      } else if (type === 'player') {
+        const invertedBindings = R.invertObj(bindings);
+        const characterName = invertedBindings[fromName];
+        if (characterName !== undefined) {
+          bindings[characterName] = toName;
         }
-
-        addListener('removeProfile', _removeProfile);
+      } else {
+        console.log(`binding._renameProfile: Unexpected type ${type}`);
+      }
     }
 
-    callback2(profileBindingAPI);
-})(api => (typeof exports === 'undefined' ? (this.profileBindingAPI = api) : (module.exports = api)));
+    addListener('renameProfile', _renameProfile);
+
+    function _removeProfile([{ type, characterName }] = []) {
+      const bindings = R.path(path, this.database);
+      if (type === 'character') {
+        delete bindings[characterName];
+      } else if (type === 'player') {
+        const invertedBindings = R.invertObj(bindings);
+        const characterName2 = invertedBindings[characterName];
+        if (characterName2 !== undefined) {
+          delete bindings[characterName2];
+        }
+      } else {
+        console.log(`binding._removeProfile: Unexpected type ${type}`);
+      }
+    }
+
+    addListener('removeProfile', _removeProfile);
+  }
+
+  callback2(profileBindingAPI);
+})((api) => (typeof exports === 'undefined' ? (this.profileBindingAPI = api) : (module.exports = api)));
